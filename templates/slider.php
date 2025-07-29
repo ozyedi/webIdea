a70751e62f569d30b8ec65bc9ee54c88884df80e
Nihat <nihatozyedi@gmail.com>
2025-07-29 22:53:43 +0300

    Slider zoom problemi ve metin sabitleme dahil stil ve yapı güncellemesi

--- templates/slider.php
+++ templates/slider.php
@@ -2,31 +2,34 @@
 <div class="slider">
     
     <div class="slides"><!-- start slides -->
+
         <div class="slide active">
-        <img src="https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Slide 1">
-            <div class="slide-meta">
-                <h4>Slide Başlığı 1</h4>
-                <p>Açıklama metni burada olacak.</p>
-            </div>
-        </div>
-
-  
-        <div class="slide">
-        <img src="https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Slide 2">
-            <div class="slide-meta">
-                <h4>Slide Başlığı 2</h4>
-                <p>Açıklama metni burada olacak.</p>
-            </div>
+          <div class="slide-bg" style="background-image: url('https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');"></div>
+          <div class="slide-meta">
+            <h4>Başlık 1</h4>
+            <p>Açıklama metni 1</p>
+          </div>
         </div>
 
 
-        <div class="slide">
-        <img src="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Slide 3">
-            <div class="slide-meta">
-                <h4>Slide Başlığı 3</h4>
-                <p>Açıklama metni burada olacak.</p>
-            </div>
+
+        <div class="slide active">
+          <div class="slide-bg" style="background-image: url('https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');"></div>
+          <div class="slide-meta">
+            <h4>Başlık 2</h4>
+            <p>Açıklama metni 2</p>
+          </div>
         </div>
+
+
+        <div class="slide active">
+          <div class="slide-bg" style="background-image: url('https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');"></div>
+          <div class="slide-meta">
+            <h4>Başlık 3</h4>
+            <p>Açıklama metni 3</p>
+          </div>
+        </div>
+
 
     </div><!-- End slides -->
     
